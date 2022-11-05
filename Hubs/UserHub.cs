e3e99420b567e418b21cf9faa6c3f70d07f871f4
Luis Coco Enríquez <luiscocoenriquez@hotmail.com>
2022-11-05 18:01:25 +0100

    First version

---
+++ Hubs/UserHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+    public class UserHub :  Hub
+    {
+        public static int TotalViews { get; set; }
+
+        public async Task NewWindowLoaded() 
+        {
+            TotalViews++;
+
+            //send update to all clients that total views have been updated
+            await Clients.All.SendAsync("updateTotalViews",TotalViews);
+
+
+        }
+    }
+}
